993dae4d0d4c4866359262bc621765811cbcf022
AleksPastukhov <passandro1990@gmail.com>
2023-04-10 22:02:57 +0300

    add hw 4

--- App.js
+++ App.js
@@ -1,34 +1,62 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import "react-native-gesture-handler";
+import { useCallback } from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFonts } from "expo-font";
-
-import LoginScreen from "./Screens/LoginScreen/LoginScreen";
-import RegistrationScreen from "./Screens/RegistrationScreen/RegistrationScreen";
+import * as SplashScreen from "expo-splash-screen";
+import Authorization from "./src/screens/Auth/Authorization/Authorization";
+import Home from "./src/screens/main/Home/Home";
 
 export default function App() {
-  console.log("Hello debugger!!!");
+  const Stack = createNativeStackNavigator();
+
+  // For fonts instalation
+  SplashScreen.preventAutoHideAsync();
 
   const [fontsLoaded] = useFonts({
-    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
-    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Regular": require("./src/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./src/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./src/fonts/Roboto-Bold.ttf"),
   });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  onLayoutRootView();
 
   if (!fontsLoaded) {
     return null;
   }
 
+  // End of Fonta instalation
+
   return (
-    <View style={styles.container}>
-      <RegistrationScreen />
-      {/* <LoginScreen /> */}
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Authorization"
+          component={Authorization}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    position: "relative",
     flex: 1,
+    backgroundColor: "#fff",
+    fontFamily: "Roboto-Regular",
   },
 });
